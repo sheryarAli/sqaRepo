c45b3f1e09dbfbf7a07c9eff81c652c0b0fcdc50
Shahzore <shahzore.ali@mfb.com.pk>
2020-03-09 12:21:58 +0500

    Add files

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     dburl: 'mongodb+srv://shahzore:<12345>@cluster0-1kwdo.mongodb.net/test?retryWrites=true&w=majority',
+    
     sessionSecret: 'Fmfb@2019',
     ldap: {
         dn: 'CN=Person,CN=Schema,CN=Configuration,DC=um,DC=com',
