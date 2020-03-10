e7d28bffa93c0b8cdaf424b5adb1bf48c80e4023
Shahzore <shahzore.ali@mfb.com.pk>
2020-03-10 15:14:03 +0500

    Commit Files

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://shahzore:<12345>@cluster0-shard-00-00-1kwdo.mongodb.net:27017,cluster0-shard-00-01-1kwdo.mongodb.net:27017,cluster0-shard-00-02-1kwdo.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+    mongoURI: 'mongodb://shahzore:12345@cluster0-shard-00-00-1kwdo.mongodb.net:27017,cluster0-shard-00-01-1kwdo.mongodb.net:27017,cluster0-shard-00-02-1kwdo.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 }
